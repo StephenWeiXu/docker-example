d08350bb3bb0b7c905f22b34ad8a2df20fb1ea94
Wei Xu <wei.xu@ithaka.org>
2018-02-02 08:17:19 -0500

    fix app.js server listening

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,7 @@
 const http = require("http");
 
-http.createServer((req, res)=>res.end(""hi!)):
+http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end("<h1>This is just a demo</h1>");
+}).listen(3000);
   
